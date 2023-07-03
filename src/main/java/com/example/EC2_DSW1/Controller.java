136a329fca005720a56a3c8f43da39e2a6b26bef
LuisCarBl <134304449+LuisCarBl@users.noreply.github.com>
2023-07-03 13:32:58 +0000

    pal render

---
+++ src/main/java/com/example/EC2_DSW1/Controller.java
@@ -0,0 +1,17 @@
+package com.example.EC2_DSW1;
+
+import java.lang.String;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping(path="/")
+
+public class Controller {
+
+    @GetMapping(path="/")
+    public String home() {
+        return "LN90161994 - Luis, Carmen Blas";
+    }
+}
---
+++ src/test/java/com/example/EC2_DSW1/Ec2Dsw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EC2_DSW1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2Dsw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
